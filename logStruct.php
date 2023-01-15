ae134e37cceb2c293ec569f82f3d39ea0e710610
rakesh ramteke <100757764+fab-rakesh@users.noreply.github.com>
2023-01-15 22:27:33 +0530

    Add task list and hr

--- logStruct.php
+++ logStruct.php
@@ -6,7 +6,11 @@ $days = 100; // 100 days of challenge
 for ($i = 0; $i <= $days; $i++) {
   $date = strtotime("+$i day", strtotime("2023-01-15"));
   echo "### Day $i (" . date('l jS M Y', $date) . ")";
-  echo "\n**Today's Progress:**";
-  echo "\n\n**Thoughts:**";
-  echo "\n\n**Link (s):** [text](link)\n\n\n";
+  echo "\n**Today's Progress:**\n\n";
+  echo "- [ ] Progress posted on Twitter\n";
+  echo "- [ ] Code pushed on GitHub\n";
+  echo "\n**Thoughts:**";
+  echo "\n\n**Link(s):** [text](link)\n\n";
+  if ($i != $days)
+    echo "<hr>\n\n";
 }
